84f249eb198333234c38606ea4cb99722d1be225
unknown <9004049287@so-net.co.jp>
2017-12-22 17:53:02 +0900

    ググる試行

--- app/http_access_study.py
+++ app/http_access_study.py
@@ -1,32 +1,40 @@
 # -*- coding: utf-8 -*-
-import httplib2
-import urllib.robotparser
-import reppy.robots
-import bs4
+from httplib2 import Http
+from urllib.robotparser import RobotFileParser
+#from reppy.robots import Robots
+from bs4 import BeautifulSoup
+from google import search
 
-SAMPLE_URL = 'https://ja.wikipedia.org/wiki/%E3%82%A2%E3%83%A1%E3%83%AA%E3%82%AB%E8%88%AA%E7%A9%BA%E5%AE%87%E5%AE%99%E5%B1%80'
+# SAMPLE_URL = 'https://ja.wikipedia.org/wiki/%E3%82%A2%E3%83%A1%E3%83%AA%E3%82%AB%E8%88%AA%E7%A9%BA%E5%AE%87%E5%AE%99%E5%B1%80'
 # SAMPLE_URL = 'http://www.cyclowired.jp/robots.txt'
+SAMPLE_URL = 'https://www.google.co.jp/search?q=integer'
 
 def http_req():
-    h = httplib2.Http()
-    headers = {'cache-control':'no-cache'}
-    (response, content) = h.request(SAMPLE_URL, headers=headers)
+
+    for url in search('腰痛', stop=20, lang='ja'):
+        print (url)
+        # soup = BeautifulSoup(urllib.urlopen(url))
+        # print soup.find("title").text
+
+    # h = Http()
+    # headers = {}
+    # (response, content) = h.request(SAMPLE_URL, headers=headers)
 
     # items = dict(response.items())
-    for key,value in response.items():
+    # for key,value in response.items():
 
-        if (key != 'content-type'):
-            pass
-        else:
-            print(key + '-------------- ' + value)
-
-    soup = bs4.BeautifulSoup(content, "html.parser")
-    aList = soup.find_all('a')
-    for a in aList:
-        print (a)
+    #     if (key != 'content-type'):
+    #         pass
+    #     else:
+    #         print(key + '-------------- ' + value)
+    # print (content)
+    # soup = BeautifulSoup(content, "html.parser")
+    # aList = soup.find_all('p')
+    # for a in aList:
+    #     print (a)
 
 def robot_req():
-    robots = reppy.robots.Robots.fetch(SAMPLE_URL + 'robots.txt')
+    robots = Robots.fetch(SAMPLE_URL + 'robots.txt')
     agent = robots.agent('hogehoge-agent')
     print (agent.allowed(SAMPLE_URL + 'w/load.php?'))
     print (agent.delay)
