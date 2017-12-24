bebcbaa318b293c12baa4f3aa94b4c73aceacd80
yoshioH <yoshio@hirayamahiroshitakenoMacBook-Pro.local>
2017-12-24 22:55:07 +0900

    google searchを作成c

--- app/http_access_study.py
+++ app/http_access_study.py
@@ -4,15 +4,33 @@ from urllib.robotparser import RobotFileParser
 #from reppy.robots import Robots
 from bs4 import BeautifulSoup
 from google import search
+from http.cookiejar import LWPCookieJar
+from urllib.request import Request, urlopen
+import os
 
 # SAMPLE_URL = 'https://ja.wikipedia.org/wiki/%E3%82%A2%E3%83%A1%E3%83%AA%E3%82%AB%E8%88%AA%E7%A9%BA%E5%AE%87%E5%AE%99%E5%B1%80'
 # SAMPLE_URL = 'http://www.cyclowired.jp/robots.txt'
-SAMPLE_URL = 'https://www.google.co.jp/search?q=integer'
+SAMPLE_URL = 'http://www.google.co.jp/search?q=integer'
+
+home_folder = os.getenv('HOME')
+if not home_folder:
+    home_folder = os.getenv('USERHOME')
+    if not home_folder:
+        home_folder = '.'   # Use the current folder on error.
+cookie_jar = LWPCookieJar(os.path.join(home_folder, '.google-cookie'))
+print (os.path.join(home_folder, '.google-cookie'))
+try:
+    cookie_jar.load()
+except Exception:
+    pass
+USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)'
 
 def http_req():
 
-    for url in search('腰痛', stop=20, lang='ja'):
-        print (url)
+    html = googe_search(SAMPLE_URL)
+    # print (html)
+    # for url in search('腰痛', stop=20, lang='ja'):
+    #     print (url)
         # soup = BeautifulSoup(urllib.urlopen(url))
         # print soup.find("title").text
 
@@ -32,6 +50,17 @@ def http_req():
     # aList = soup.find_all('p')
     # for a in aList:
     #     print (a)
+
+def googe_search(url:str):
+    request = Request(url)
+    request.add_header('User-Agent', USER_AGENT)
+    cookie_jar.add_cookie_header(request)
+    response = urlopen(request)
+    cookie_jar.extract_cookies(response, request)
+    html = response.read()
+    response.close()
+    cookie_jar.save()
+    return html
 
 def robot_req():
     robots = Robots.fetch(SAMPLE_URL + 'robots.txt')
