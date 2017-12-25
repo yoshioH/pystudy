49cd88588c13e245c817f9ca40667add10b49250
unknown <9004049287@so-net.co.jp>
2017-12-25 18:15:59 +0900

    update

--- app/http_access_study.py
+++ app/http_access_study.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from httplib2 import Http
 from urllib.robotparser import RobotFileParser
+from html.parser import unescape
+# from urllib.parse import url
 #from reppy.robots import Robots
 from bs4 import BeautifulSoup
+from bs4 import UnicodeDammit
 from google import search
 from http.cookiejar import LWPCookieJar
 from urllib.request import Request, urlopen
@@ -18,7 +21,7 @@ if not home_folder:
     if not home_folder:
         home_folder = '.'   # Use the current folder on error.
 cookie_jar = LWPCookieJar(os.path.join(home_folder, '.google-cookie'))
-print (os.path.join(home_folder, '.google-cookie'))
+# print (os.path.join(home_folder, '.google-cookie'))
 try:
     cookie_jar.load()
 except Exception:
@@ -28,6 +31,28 @@ USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)'
 def http_req():
 
     html = googe_search(SAMPLE_URL)
+
+
+    soup = BeautifulSoup(html, 'lxml')
+    aList = soup.find_all("a")
+
+    with open('hoge.html', 'wb') as file:
+        for a in aList:
+            file.write(a.encode())
+
+    # dammit = UnicodeDammit(html)
+
+
+    # print(dammit.unicode_markup)
+    # print(dammit.original_encoding)
+
+    # print (html.decode(encoding='utf-8', errors='ignore'))
+    # soup = BeautifulSoup(html, "html.parser")
+    # print (soup.prettify())
+    # aList = soup.find_all('p')
+    # for a in aList:
+    #     print (a)
+
     # print (html)
     # for url in search('腰痛', stop=20, lang='ja'):
     #     print (url)
@@ -55,12 +80,15 @@ def googe_search(url:str):
     request = Request(url)
     request.add_header('User-Agent', USER_AGENT)
     cookie_jar.add_cookie_header(request)
-    response = urlopen(request)
-    cookie_jar.extract_cookies(response, request)
-    html = response.read()
-    response.close()
-    cookie_jar.save()
-    return html
+    # GET
+    with urlopen(request) as response:
+        # cookieの保管
+        cookie_jar.extract_cookies(response, request)
+        html = response.read()
+        # cookieをファイル保管
+        cookie_jar.save()
+        return html
+    return ''
 
 def robot_req():
     robots = Robots.fetch(SAMPLE_URL + 'robots.txt')
